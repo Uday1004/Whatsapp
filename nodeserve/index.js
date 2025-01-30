ec726c709777c8fe95847fcf844f7cfc481d3b2f
Uday Solanki <Udaysolanki530@gmail.com>
2025-01-30 14:23:40 +0530

    update

--- nodeserve/index.js
+++ nodeserve/index.js
@@ -1,26 +1,68 @@
-// node server for whatsapp clone which will handle socket io connection
-const io = require('socket.io')(8000)
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
 
-const users={};
+// Create an HTTP server
+const server = http.createServer();
 
-io.on('connection',socket=> {   
-    //if any user want to join it take name and store in form of id      
-    socket.on('new-user-joined', name => {     
-        console.log('New user', name)
-        users[socket.id]= name;              
-        socket.broadcast.emit('user-joined', name);
-    });
+// Initialize Socket.IO with CORS configuration
+const io = socketIo(server, {
+  cors: {
+    origin: '*', // Allow all origins (replace with your frontend URL in production)
+    methods: ['GET', 'POST'], // Allowed HTTP methods
+  },
+});
 
-    //if someone sends a message ,boradcast to other joined people
-    socket.on('send',message => {             
-        socket.broadcast.emit('receive',{message: message, name:users[socket.id]});
-    });
+// Store connected users
+const users = {};
 
-    //when anybody left ,broadcast to others user
-    socket.on('disconnect',message => {       
-        socket.broadcast.emit('left',users[socket.id]);
-        delete users[socket.id];
-    });    
-})
- 
- 
+// Handle Socket.IO connections
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  // When a new user joins
+  socket.on('new-user-joined', (name) => {
+    if (!name) {
+      console.log('Invalid name provided');
+      return;
+    }
+
+    console.log('New user joined:', name);
+    users[socket.id] = name;
+
+    // Broadcast to all other users that a new user has joined
+    socket.broadcast.emit('user-joined', name);
+  });
+
+  // When a user sends a message
+  socket.on('send', (message) => {
+    const senderName = users[socket.id];
+    if (!senderName) {
+      console.log('Unknown user tried to send a message');
+      return;
+    }
+
+    // Broadcast the message to all other users
+    socket.broadcast.emit('receive', { message, name: senderName });
+  });
+
+  // When a user disconnects
+  socket.on('disconnect', () => {
+    const userName = users[socket.id];
+    if (userName) {
+      console.log('User left:', userName);
+
+      // Broadcast to all other users that this user has left
+      socket.broadcast.emit('left', userName);
+
+      // Remove the user from the `users` object
+      delete users[socket.id];
+    }
+  });
+});
+
+// Start the server
+const PORT = 8000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
